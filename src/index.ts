c59d31392a355a7968debd541a26aaeb2cdb1a78
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-09-05 17:45:49 -0300

    feat: add aliases to commands

--- src/index.ts
+++ src/index.ts
@@ -3,36 +3,30 @@ import chalk from "chalk";
 import yargs from "yargs";
 
 import figmaGenerate from "./commands/figma-generate";
-import wefitLogo from "./constants/wefitLogo";
 import translationExport from "./commands/translatation-export";
 import translationImport from "./commands/translation-import";
+import wefitLogo from "./constants/wefitLogo";
 
 console.log(chalk.yellow(wefitLogo));
 
 yargs(process.argv.slice(2))
-  .command(
-    "figma-generate",
-    "Criar arquivo de estilos base",
-    (yargs) => {},
-    (args) => {
-      figmaGenerate({});
-    }
-  )
-  .command(
-    "translation-export",
-    "Exporta os arquivos da pasta locales para xlsx",
-    () => {},
-    () => {
-      translationExport();
-    }
-  )
-  .command(
-    "translation-import",
-    "Importa a planilha de tradução",
-    () => {},
-    () => {
-      translationImport();
-    }
-  )
+  .command({
+    command: "figma-generate",
+    describe: "Criar arquivo de estilos base",
+    handler: () => figmaGenerate({}),
+    aliases: ["figma-generate", "fg"],
+  })
+  .command({
+    command: "translation-export",
+    describe: "Exporta os arquivos da pasta locales para xlsx",
+    handler: () => translationExport(),
+    aliases: ["translation-export", "te"],
+  })
+  .command({
+    command: "translation-import",
+    describe: "Importa a planilha de tradução",
+    handler: () => translationImport(),
+    aliases: ["translation-import", "ti"],
+  })
   .help("h")
   .alias("h", "help").argv;
