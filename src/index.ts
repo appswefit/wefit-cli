437479404cde855a4f0389735b6dd3eb1bb70fa9
José Eduardo Rodrigues Pinto Jerp86 <jerp.dev@gmail.com>
2023-09-06 00:16:20 -0300

    feat: add command and examples

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import chalk from "chalk";
 import yargs from "yargs";
 
 import figmaGenerate from "./commands/figma-generate";
+import setGitRemoteCredential from "./commands/setGitCredential";
 import translationExport from "./commands/translatation-export";
 import translationImport from "./commands/translation-import";
 import wefitLogo from "./constants/wefitLogo";
@@ -28,6 +29,15 @@ yargs(process.argv.slice(2))
     handler: () => translationImport(),
     aliases: ["translation-import", "ti"],
   })
+  .command({
+    command: "set-git-credential",
+    describe: "Seta a nova credencial para o repositório",
+    handler: () => {
+      const credential = process.argv[3];
+      setGitRemoteCredential(credential);
+    },
+    aliases: ["setGitCredential", "sgc"],
+  })
   .example([
     ["$0 figma-generate", "Criar arquivo de estilos base"],
     ["$0 fg", "Short syntax\n"],
@@ -37,6 +47,12 @@ yargs(process.argv.slice(2))
 
     ["$0 translation-import", "Importa a planilha de tradução"],
     ["$0 ti", "Short syntax\n"],
+
+    [
+      "$0 setGitCredential yourNewCredential",
+      "Atualiza a credencial do repositório",
+    ],
+    ['$0 sgc "your New Credential"', "Short syntax\n"],
   ])
   .help("h")
   .alias("h", "help").argv;
